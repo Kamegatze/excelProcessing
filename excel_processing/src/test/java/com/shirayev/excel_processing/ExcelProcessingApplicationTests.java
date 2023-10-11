e3f578720c5906014fa7a15ef9e98b3919ce76ae
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-11 08:43:03 +0300

    'init'

---
+++ excel_processing/src/test/java/com/shirayev/excel_processing/ExcelProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirayev.excel_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
