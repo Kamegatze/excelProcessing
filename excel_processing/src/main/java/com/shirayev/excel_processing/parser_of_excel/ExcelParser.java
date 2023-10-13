c838967b7e7ac16de34df428cc5499e6b74fa58d
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-13 10:02:38 +0300

    добавление функции getSheets в ExcelParser

--- excel_processing/src/main/java/com/shirayev/excel_processing/parser_of_excel/ExcelParser.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/parser_of_excel/ExcelParser.java
@@ -1,8 +1,81 @@
 package com.shirayev.excel_processing.parser_of_excel;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
 @Component
 public class ExcelParser {
+
+    private List<Map<String, String>> getSheet(Sheet sheet, Boolean withATitle) {
+        Iterator<Row> rowIterator = sheet.iterator();
+
+        Row row = rowIterator.next();
+
+        List<String> keys = new ArrayList<>();
+
+        if(withATitle) {
+
+            for (Iterator<Cell> cellIterator = row.cellIterator();
+                 cellIterator.hasNext();) {
+
+                Cell cell = cellIterator.next();
+
+                keys.add(cell.getStringCellValue());
+
+            }
+        }
+
+        List<Map<String, String>> records = new ArrayList<>();
+
+        while (rowIterator.hasNext()) {
+            row = rowIterator.next();
+
+            Map<String, String> record = new LinkedHashMap<>();
+
+            int index = 0;
+
+            for(Iterator<Cell> cellIterator = row.cellIterator();
+                cellIterator.hasNext();) {
+
+                Cell cell = cellIterator.next();
+
+                if(withATitle) {
+                    record.put(keys.get(index), cell.getStringCellValue());
+                } else {
+                    record.put(String.valueOf(index), cell.getStringCellValue());
+                }
+
+                index++;
+            }
+
+            records.add(record);
+
+        }
+
+        return records;
+    }
+
+    public Map<String, List<Map<String, String>>> getSheets(String path, Boolean withATitle) throws IOException {
+        Workbook book = new HSSFWorkbook(new FileInputStream(path));
+
+        Map<String, List<Map<String, String>>> sheets = new HashMap<>();
+
+        for(Iterator<Sheet> sheetIterator = book.sheetIterator();
+            sheetIterator.hasNext();) {
+            Sheet sheet = sheetIterator.next();
+
+            sheets.put(sheet.getSheetName(), this.getSheet(sheet, withATitle));
+        }
+
+        return sheets;
+    }
 
 }
