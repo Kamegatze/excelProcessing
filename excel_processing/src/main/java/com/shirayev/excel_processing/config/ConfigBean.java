44278fb7f5cf5cba4ff63b143516d27032de6129
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-16 10:45:23 +0300

    добавление конфигурации бина и зависимости для ModelMapper

---
+++ excel_processing/src/main/java/com/shirayev/excel_processing/config/ConfigBean.java
@@ -0,0 +1,22 @@
+package com.shirayev.excel_processing.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigBean {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        return mapper;
+    }
+}
