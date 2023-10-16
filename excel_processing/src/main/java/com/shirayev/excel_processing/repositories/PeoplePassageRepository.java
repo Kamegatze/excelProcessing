0fa3592bc38a2c9cadeed7d3866dc24a4cd38363
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-16 08:16:53 +0300

    Добавление интерфейса PeoplePassageRepository в качестве ропозитория для сущности PeoplePassage

---
+++ excel_processing/src/main/java/com/shirayev/excel_processing/repositories/PeoplePassageRepository.java
@@ -0,0 +1,7 @@
+package com.shirayev.excel_processing.repositories;
+
+import com.shirayev.excel_processing.entities.PeoplePassage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeoplePassageRepository extends JpaRepository<PeoplePassage, Integer> {
+}
