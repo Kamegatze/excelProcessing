db701e104fcfa59306f3d7b8493b9a95b65ebc33
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-17 10:32:06 +0300

    Добавление SheetsService

---
+++ excel_processing/src/main/java/com/shirayev/excel_processing/servicies/SheetsService.java
@@ -0,0 +1,31 @@
+package com.shirayev.excel_processing.servicies;
+
+import com.shirayev.excel_processing.dto.SheetsDto;
+import com.shirayev.excel_processing.repositories.SheetsRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SheetsService {
+
+    private final SheetsRepository sheetsRepository;
+
+    private final ModelMapper model;
+
+    public List<SheetsDto> getSheets() {
+        return SheetsDto.getSheetsDto(sheetsRepository.findAll());
+    }
+
+    public SheetsDto getSheetById(Long id) {
+        return model.map(sheetsRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Листа с id: " + id + " не существует")
+        ), SheetsDto.class);
+    }
+}
