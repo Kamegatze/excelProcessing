01d886656d086328d1e57a28f47c81c6ae326d12
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-10-23 11:23:16 +0300

    Рефаеторинг ETypeError

--- excel_processing/src/main/java/com/shirayev/excel_processing/dto/error/ETypeError.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/dto/error/ETypeError.java
@@ -5,6 +5,13 @@ public enum ETypeError {
     TITLE_EXCEPTION(2),
     EMPTY_VALUE_RECORD_EXCEPTION(3);
 
+    private final Integer code;
+
     ETypeError(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
     }
 }
