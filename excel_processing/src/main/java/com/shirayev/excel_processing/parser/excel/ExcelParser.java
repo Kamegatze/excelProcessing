7bf61a8a9aeaffc00f71cb4b3a7ab1d8e0100ae2
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-10-23 10:41:57 +0300

    Добавление проверки на пустые поля

--- excel_processing/src/main/java/com/shirayev/excel_processing/parser/excel/ExcelParser.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/parser/excel/ExcelParser.java
@@ -1,10 +1,12 @@
 package com.shirayev.excel_processing.parser.excel;
 
+import com.shirayev.excel_processing.custom_exception.EmptyValueInRecordException;
 import com.shirayev.excel_processing.custom_exception.TitleException;
 import com.shirayev.excel_processing.dto.PeoplePassageDto;
 import com.shirayev.excel_processing.dto.SheetsDto;
 import com.shirayev.excel_processing.parser.Parser;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -21,6 +23,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 @Data
 @Component
@@ -31,7 +34,7 @@ public class ExcelParser implements Parser<List<SheetsDto>> {
     private final List<String> fields = List.of("last_name", "first_name", "patronymic",
             "age", "actions", "time_action");
 
-    private List<PeoplePassageDto> getSheet(Sheet sheet) throws TitleException {
+    private List<PeoplePassageDto> getSheet(Sheet sheet) throws TitleException, EmptyValueInRecordException {
 
         Iterator<Row> rowIterator = sheet.iterator();
         Row row;
@@ -80,16 +83,36 @@ public class ExcelParser implements Parser<List<SheetsDto>> {
         /*
         * Обработать случай когда поля будут пустыми
         * */
-        return records.stream().map(item ->
-            PeoplePassageDto.builder()
-                    .age(Integer.valueOf(item.get("age")))
-                    .action(item.get("actions"))
-                    .timeAction(Time.valueOf(item.get("time_action")))
-                    .last_name(item.get("last_name"))
-                    .first_name(item.get("first_name"))
-                    .patronymic(item.get("patronymic"))
-                    .build()
-        ).toList();
+
+        List<PeoplePassageDto> result = new ArrayList<>();
+
+        for(Map<String, String> record : records) {
+            result.add(checkEmptyValueInRecord(record));
+        }
+
+        return result;
+    }
+
+    private PeoplePassageDto checkEmptyValueInRecord(Map<String, String> record) throws EmptyValueInRecordException {
+        boolean isRecordValid = !(record.get("age").isEmpty() || record.get("age").isBlank())
+                && !(record.get("actions").isBlank() || record.get("actions").isEmpty())
+                && !(record.get("time_action").isBlank() || record.get("time_action").isEmpty())
+                && !(record.get("last_name").isBlank() || record.get("last_name").isEmpty())
+                && !(record.get("first_name").isBlank() || record.get("first_name").isEmpty());
+
+        if (!isRecordValid) {
+            throw new EmptyValueInRecordException("Проверьте ваши записи на пустые значения. Поля: age, actions, time_actions, last_name, first_name, " +
+                    "обязательны к заполнению");
+        }
+
+        return PeoplePassageDto.builder()
+                .age(Integer.valueOf(record.get("age")))
+                .action(record.get("actions"))
+                .timeAction(Time.valueOf(record.get("time_action")))
+                .last_name(record.get("last_name"))
+                .first_name(record.get("first_name"))
+                .patronymic(record.get("patronymic"))
+                .build();
     }
 
     private Boolean checkTitle(Sheet sheet) throws TitleException {
