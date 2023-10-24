ba00e918ab5c706377c2f2909abad796f7e2af8f
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-24 09:28:26 +0300

    Добавление общего advice для обработки исключения NoSuchElementException

---
+++ excel_processing/src/main/java/com/shirayev/excel_processing/advice/CommonAdvice.java
@@ -0,0 +1,26 @@
+package com.shirayev.excel_processing.advice;
+
+import com.shirayev.excel_processing.dto.error.ETypeError;
+import com.shirayev.excel_processing.dto.error.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class CommonAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorDto> handlerNoSuchElementException(NoSuchElementException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ErrorDto.builder()
+                        .message(exception.getMessage())
+                        .code(ETypeError.NO_SUCH_ELEMENT_EXCEPTION.getCode())
+                        .build());
+    }
+
+}
--- excel_processing/src/main/java/com/shirayev/excel_processing/dto/error/ETypeError.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/dto/error/ETypeError.java
@@ -3,7 +3,8 @@ package com.shirayev.excel_processing.dto.error;
 public enum ETypeError {
     IO_EXCEPTION(1),
     TITLE_EXCEPTION(2),
-    EMPTY_VALUE_RECORD_EXCEPTION(3);
+    EMPTY_VALUE_RECORD_EXCEPTION(3),
+    NO_SUCH_ELEMENT_EXCEPTION(4);
 
     private final Integer code;
 
