e7e2807ee97e5c12c3ec870f4bdc3089f711baf0
Alexey Shiryaev <sap@smart.inreco.ru>
2023-10-30 11:53:14 +0300

    Добавление метода для поиска всех недостающих файлов в микросервисе

--- excel_processing/src/main/java/com/shirayev/excel_processing/repositories/FileRepository.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/repositories/FileRepository.java
@@ -3,6 +3,10 @@ package com.shirayev.excel_processing.repositories;
 import com.shirayev.excel_processing.entities.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    List<File> findByIdNotIn(List<Integer> ids);
 
 }
