0228e938e5132e0cb6d3e49c9220509bf0fdcebc
Alexey Shiryaev <sap@smart.inreco.ru>
2023-11-01 11:48:26 +0300

    добавление методов для сохранения сущности StatisticsPeoplePassage и метод для получения статистики за определенный период

--- statitistics_people_passage/src/main/java/com/shirayev/statistics_people_passage/servicies/StatisticsPeoplePassageService.java
+++ statitistics_people_passage/src/main/java/com/shirayev/statistics_people_passage/servicies/StatisticsPeoplePassageService.java
@@ -1,15 +1,22 @@
 package com.shirayev.statistics_people_passage.servicies;
 
-import com.shirayev.statistics_people_passage.dto.page.PageDto;
 import com.shirayev.statistics_people_passage.dto.StatisticsPeoplePassageDto;
+import com.shirayev.statistics_people_passage.dto.page.PageRequestDto;
+import com.shirayev.statistics_people_passage.entities.Sheets;
 import com.shirayev.statistics_people_passage.entities.StatisticsPeoplePassage;
+import com.shirayev.statistics_people_passage.model.AvgAgeGroupByActionStatisticsPeoplePassage;
 import com.shirayev.statistics_people_passage.repositories.StatisticsPeoplePassageRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,18 +25,51 @@ public class StatisticsPeoplePassageService {
 
     private final StatisticsPeoplePassageRepository statisticsPeoplePassageRepository;
 
-    private final ModelMapper modelMapper;
-
+    private final ModelMapper model;
     @Transactional
-    public PageDto<StatisticsPeoplePassageDto> updateAndInsertOfData(PageDto<StatisticsPeoplePassageDto> statisticsPeoplePassageDtoPageDto) {
+    public List<StatisticsPeoplePassageDto> updateAndInsertOfData(List<StatisticsPeoplePassageDto> statisticsPeoplePassageDtoList, Sheets sheets) {
         List<StatisticsPeoplePassage> statisticsPeoplePassages = StatisticsPeoplePassageDto
-                .getStatisticsPeoplePassageEntity(statisticsPeoplePassageDtoPageDto.getContent());
+                .getStatisticsPeoplePassageEntity(statisticsPeoplePassageDtoList);
+
+        statisticsPeoplePassages.forEach(item -> {
+            item.setSheet(sheets);
+        });
 
         statisticsPeoplePassages = statisticsPeoplePassageRepository.saveAll(statisticsPeoplePassages);
 
-        statisticsPeoplePassageDtoPageDto.setContent(StatisticsPeoplePassageDto.getStatisticsPeoplePassageDto(statisticsPeoplePassages));
+        return StatisticsPeoplePassageDto.getStatisticsPeoplePassageDto(statisticsPeoplePassages);
+    }
 
-        return statisticsPeoplePassageDtoPageDto;
+    public List<AvgAgeGroupByActionStatisticsPeoplePassage> getStatisticsByActionAndAge(PageRequestDto pageRequestDto, Time start, Time end) {
+        Page<StatisticsPeoplePassage> pageStatisticsPeoplePassage = statisticsPeoplePassageRepository.findAllByTimeActionBetween(start, end,
+                PageRequestDto.getPageRequest(pageRequestDto));
+
+        List<StatisticsPeoplePassage> statisticsPeoplePassages = pageStatisticsPeoplePassage.getContent();
+
+        /*
+        * Нахождение среднего возраста по действию
+        * */
+        Map<String, Double> statistics = statisticsPeoplePassages.stream().collect(
+                Collectors.groupingBy(
+                        StatisticsPeoplePassage::getAction,
+                        Collectors.averagingInt(StatisticsPeoplePassage::getAge
+                        )
+                )
+        );
+        /*
+        * Формирование отсвета метода
+        * */
+        List<AvgAgeGroupByActionStatisticsPeoplePassage> avgAgeGroupByActionStatisticsPeoplePassages = new ArrayList<>();
+
+        statistics.forEach((key, value) -> {
+            avgAgeGroupByActionStatisticsPeoplePassages.add(AvgAgeGroupByActionStatisticsPeoplePassage.builder()
+                    .actions(key)
+                    .age(value)
+                    .build()
+            );
+        });
+
+        return avgAgeGroupByActionStatisticsPeoplePassages;
     }
 
 }
