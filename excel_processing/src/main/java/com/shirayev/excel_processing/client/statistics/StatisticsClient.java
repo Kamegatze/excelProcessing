76e5681210114856291f94b530ae4f38b9ee929a
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-11-03 09:20:41 +0300

    Изменение логики отправки

--- excel_processing/src/main/java/com/shirayev/excel_processing/client/statistics/StatisticsClient.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/client/statistics/StatisticsClient.java
@@ -4,8 +4,12 @@ import com.shirayev.excel_processing.client_micro_service.TransferDataClient;
 import com.shirayev.excel_processing.client_micro_service.uri.URIBuilder;
 import com.shirayev.excel_processing.dto.FileDto;
 import com.shirayev.excel_processing.entities.File;
+import com.shirayev.excel_processing.repositories.FileRepository;
+import com.shirayev.excel_processing.servicies.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.NoSuchElementException;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +19,17 @@ public class StatisticsClient {
 
     private final TransferDataClient<FileDto> statisticsTransfer;
 
-    public FileDto handlerTransferData(File file) {
+    private FileRepository fileRepository;
+
+    public FileDto handlerTransferData(FileDto fileDto) {
+
+        Long fileId = fileDto.getId();
+
+        File file = fileRepository.findById(fileId)
+                .orElseThrow(
+                        () -> new NoSuchElementException("Файл с id: " + fileId + " не был найден")
+                );
+
         return statisticsTransfer.transferData(uriStatistics.createURI("/save"),
                 file,
                 FileDto.class
