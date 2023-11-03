4e5d1ba487f5ddf3e523e1bac976795c495a616a
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-11-03 09:45:33 +0300

    Реализация сохранения

--- statitistics_people_passage/src/main/java/com/shirayev/statistics_people_passage/servicies/FileService.java
+++ statitistics_people_passage/src/main/java/com/shirayev/statistics_people_passage/servicies/FileService.java
@@ -1,9 +1,14 @@
 package com.shirayev.statistics_people_passage.servicies;
 
 import com.shirayev.statistics_people_passage.dto.FileDto;
+import com.shirayev.statistics_people_passage.dto.FileNesting;
+import com.shirayev.statistics_people_passage.dto.SheetsDto;
+import com.shirayev.statistics_people_passage.dto.SheetsNesting;
 import com.shirayev.statistics_people_passage.entities.File;
+import com.shirayev.statistics_people_passage.entities.Sheets;
 import com.shirayev.statistics_people_passage.repositories.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +21,12 @@ public class FileService {
 
     private final FileRepository fileRepository;
 
+    private final ModelMapper model;
+
+    private final SheetsService sheetsService;
+
+    private final StatisticsPeoplePassageService statisticsPeoplePassageService;
+
     @Transactional
     public List<FileDto> updateAndInsertOfData(List<FileDto> fileDtoList) {
         List<File> files = FileDto.getFileEntity(fileDtoList);
@@ -23,6 +34,20 @@ public class FileService {
         files = fileRepository.saveAllAndFlush(files);
 
         return FileDto.getFileDto(files);
+    }
+
+    @Transactional
+    public FileDto save(FileNesting fileNesting) {
+        File file = fileRepository.save(model.map(fileNesting, File.class));
+
+        sheetsService.updateAndInsertOfData(SheetsNesting.getSheetsDto(fileNesting.getSheets()), file);
+
+        fileNesting.getSheets().forEach(sheet -> statisticsPeoplePassageService.updateAndInsertOfData(
+                sheet.getPeoplePassages(),
+                model.map(sheet, Sheets.class)
+        ));
+
+        return model.map(file, FileDto.class);
     }
 
 }
