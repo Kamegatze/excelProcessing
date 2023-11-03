26810686c34f42f557a746918e002f458e0b18ab
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-11-03 08:37:49 +0300

    Добавление бина объекта RestTemplate

--- excel_processing/src/main/java/com/shirayev/excel_processing/config/ConfigBean.java
+++ excel_processing/src/main/java/com/shirayev/excel_processing/config/ConfigBean.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ConfigBean {
@@ -18,5 +20,10 @@ public class ConfigBean {
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
 
         return mapper;
+    }
+
+    @Bean
+    public RestOperations getRestTemplate() {
+        return new RestTemplate();
     }
 }
